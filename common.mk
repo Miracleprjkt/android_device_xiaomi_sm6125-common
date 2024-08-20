10e4c23a7881750893b2d62f3d51fbf20f36b112
Albert Tang <tangalbert919@yahoo.com>
2024-08-20 08:14:22 +0530

    sm6125-common: Import task_profiles.json
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- common.mk
+++ common.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 
